b39a6cb5c8d17c2a5edab3a258a72ef25b6e786d
Mike Sarahan <msarahan@gmail.com>
2022-08-08 16:17:06 -0500

    binary repack for osx arm64 (no cross compiling)

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
-dart pub get
-dart compile exe bin/sass.dart -o $PREFIX/bin/sass
+if [ "$BUILD_PLATFORM" = "osx-arm64" ]; then
+    # binary repack on Apple Silicon because build machines only support that via
+    #     cross compiling right now
+    mkdir -p $PREFIX/bin
+    cp sass $PREFIX/bin
+    cp src $PREFIX/bin
+else
+    dart pub get
+    dart compile exe bin/sass.dart -o $PREFIX/bin/sass
+fi
