7a3dfbfb2f590c30eeefafb4e15d55f42a577ea9
Mike Sarahan <msarahan@gmail.com>
2022-08-12 09:56:30 -0500

    try hiding rg on ci

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -16,6 +16,20 @@
 :: dart compile exe bin\sass.dart -o %LIBRARY_BIN%\sass.exe
 
 
+ECHO "bld.bat: Renaming rg.exe, see https://github.com/conda-forge/staged-recipes/issues/17519"
+SETLOCAL ENABLEEXTENSIONS
+IF ERRORLEVEL 1 ECHO "bld.bat: Unable to enable extensions: https://www.robvanderwoude.com/cmdextmsg.php"
+IF DEFINED CI (
+    IF %CI% NEQ "" (
+        move C:\Miniforge\Library\bin\rg.exe C:\Miniconda\Library\bin\rg-desactivate.exe
+    ) ELSE (
+        ECHO "bld.bat: CI is defined, but empty, so we leave rg.exe as is."
+    )
+) ELSE (
+    ECHO "bld.bat: We are not in CI, so we leave rg.exe as is."
+)
+
+
 :: Binary repack
 mkdir %LIBRARY_BIN%\src
 COPY sass.bat %LIBRARY_BIN%
